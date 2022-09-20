167b5574cb274f31ec86c6fdcc298203f6bd8c57
jcjveraa <3942301+jcjveraa@users.noreply.github.com>
2022-09-20 18:56:25 +0000

    Use Maven

---
+++ src/main/java/adventofcode/jelle/LineReader.java
@@ -0,0 +1,10 @@
+package adventofcode.jelle;
+import java.util.List;
+
+public class LineReader {
+    public static List<String> readLinesForDay(int dayNumber) {
+        
+        return null;
+        
+    }
+}
---
+++ src/test/java/adventofcode/jelle/AppTest.java
@@ -0,0 +1,20 @@
+package adventofcode.jelle;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
